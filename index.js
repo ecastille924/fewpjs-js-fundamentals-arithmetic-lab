ef9154f38cb9557bf4139ae567527570c57306f6
Erick Castille <ecastille924@gmail.com>
2021-04-10 18:07:45 -0400

    assignmemnt completed

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+var num1 = 31
+var num2 = 2
+var multiply = num1 * num2
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1
+
+var num3 = 19
+var num4 = 5
+var mod = num3 % num4
+
+var max = Math.max(13, 12, 20)
+
+
+
+
+
+
+
+// 2. Create a variable called `random` that will generate a random integer greater
+// than 0.
+// 3. Create a variable called `mod` set to an equation that will calculate the
+// remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+// 4. Create a variable called `max` that finds the highest number in a set; the
+// value returned should be `20`.
